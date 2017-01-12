b9820e5f7b88c2ec9862a4fc4ba67b315fe0e734
Josh Hicks <josh.hicks@live.com>
2017-01-12 11:37:22 -0800

    Session01 - Required Tasks

--- session01/echo_client.py
+++ session01/echo_client.py
@@ -6,20 +6,20 @@ def client(msg, log_buffer=sys.stderr):
     server_address = ('localhost', 10000)
     # TODO: Replace the following line with your code which will instantiate
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=6)
-    print('connecting to {0} port {1}'.format(*server_address), file=log_buffer)
+    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0)
+    print('connecting to {0} port {1}'.format(*server_address), file=log_buffer, flush=True)
     # TODO: connect your socket to the server here.
     sock.connect(server_address)
     # you can use this variable to accumulate the entire message received back
     # from the server
-    received_message = ''
+    received_message = b''
 
     # this try/finally block exists purely to allow us to close the socket
     # when we are finished with it
     try:
-        print('sending "{0}"'.format(msg), file=log_buffer)
+        print('sending "{0}"'.format(msg), file=log_buffer, flush=True)
         # TODO: send your message to the server here.
-        sock.sendall(msg)
+        sock.sendall(msg.encode('utf_8'))
         # TODO: the server should be sending you back your message as a series
         #       of 16-byte chunks. Accumulate the chunks you get to build the
         #       entire reply from the server. Make sure that you have received
@@ -27,16 +27,21 @@ def client(msg, log_buffer=sys.stderr):
         #
         #       Log each chunk you receive.  Use the print statement below to
         #       do it. This will help in debugging problems
-        chunk = ''
-        print('received "{0}"'.format(chunk.decode('utf8')), file=log_buffer)
+        chunk = b''
+        while 1:
+            chunk = sock.recv(16)
+            received_message += chunk
+            print('received "{0}"'.format(chunk.decode('utf8')), file=log_buffer, flush=True)
+            if len(chunk) < 16:
+                break
     finally:
         # TODO: after you break out of the loop receiving echoed chunks from
         #       the server you will want to close your client socket.
-        print('closing socket', file=log_buffer)
-
+        print('closing socket', file=log_buffer, flush=True)
+        sock.close()
         # TODO: when all is said and done, you should return the entire reply
         # you received from the server as the return value of this function.
-
+        return received_message.decode('utf8')
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
--- session01/echo_server.py
+++ session01/echo_server.py
@@ -2,39 +2,40 @@ import socket
 import sys
 
 
-def server(log_buffer=sys.stderr):
+def server(log_buffer=sys.stdout):
     # set an address for our server
     address = ('127.0.0.1', 10000)
     # TODO: Replace the following line with your code which will instantiate
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
+    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0)
     # TODO: You may find that if you repeatedly run the server script it fails,
     #       claiming that the port is already used.  You can set an option on
     #       your socket that will fix this problem. We DID NOT talk about this
     #       in class. Find the correct option by reading the very end of the
     #       socket library documentation:
     #       http://docs.python.org/3/library/socket.html#example
-
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # log that we are building a server
-    print("making a server on {0}:{1}".format(*address), file=log_buffer)
+    print("making a server on {0}:{1}".format(*address), file=log_buffer, flush=True)
 
     # TODO: bind your new sock 'sock' to the address above and begin to listen
     #       for incoming connections
-
+    sock.bind(address)
+    sock.listen(1)
     try:
         # the outer loop controls the creation of new connection sockets. The
         # server will handle each incoming connection one at a time.
         while True:
-            print('waiting for a connection', file=log_buffer)
+            print('waiting for a connection', file=log_buffer, flush=True)
 
             # TODO: make a new socket when a client connects, call it 'conn',
             #       at the same time you should be able to get the address of
             #       the client so we can report it below.  Replace the
             #       following line with your code. It is only here to prevent
             #       syntax errors
-            addr = ('bar', 'baz')
+            conn, addr = sock.accept()
             try:
-                print('connection - {0}:{1}'.format(*addr), file=log_buffer)
+                print('connection - {0}:{1}'.format(*addr), file=log_buffer, flush=True)
 
                 # the inner loop will receive messages sent by the client in
                 # buffers.  When a complete message has been received, the
@@ -45,21 +46,25 @@ def server(log_buffer=sys.stderr):
                     #       following line with your code.  It's only here as
                     #       a placeholder to prevent an error in string
                     #       formatting
-                    data = b''
+                    data = conn.recv(16)
                     print('received "{0}"'.format(data.decode('utf8')))
                     # TODO: Send the data you received back to the client, log
                     # the fact using the print statement here.  It will help in
                     # debugging problems.
+                    conn.send(data)
                     print('sent "{0}"'.format(data.decode('utf8')))
                     # TODO: Check here to see if the message you've received is
                     # complete.  If it is, break out of this inner loop.
+                    if len(data) < 16:
+                        break
 
             finally:
                 # TODO: When the inner loop exits, this 'finally' clause will
                 #       be hit. Use that opportunity to close the socket you
                 #       created above when a client connected.
+                conn.close()
                 print(
-                    'echo complete, client connection closed', file=log_buffer
+                    'echo complete, client connection closed', file=log_buffer, flush=True
                 )
 
     except KeyboardInterrupt:
@@ -67,8 +72,8 @@ def server(log_buffer=sys.stderr):
         #       close the server socket and exit from the server function.
         #       Replace the call to `pass` below, which is only there to
         #       prevent syntax problems
-        pass
-        print('quitting echo server', file=log_buffer)
+        sock.close()
+        print('quitting echo server', file=log_buffer, flush=True)
 
 
 if __name__ == '__main__':
